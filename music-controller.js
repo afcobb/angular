666f97be8f7a938f6b1c4c876f725c7601e35f6f
Alex <afcobb@gmail.com>
2017-05-25 11:12:01 -0400

    services

--- music-controller.js
+++ music-controller.js
@@ -2,22 +2,12 @@ angular
 	.module("MusicApp") // referencing the app thats created in the app-module.js
 	.controller("MusicCtrl", MusicCtrl); // creates the controller
 
-MusicCtrl.$inject = [];
+MusicCtrl.$inject = ["AppSvc", "BetaSvc"]; // this is the place where we tell the controller that it relies on the service! 
+										// we added the second service here - BetaSvc - as a string
 
-function MusicCtrl() { //this is the code in the controller
+function MusicCtrl(AppSvc, BetaSvc) { //this is the code in the controller - here we've also include the services being used
 	var self = this; // shuld always be here and first 
-	self.music = [ 
-		{ artistName: "Can", albumTitle: "Future Days", genre: "Krautrock", pubYear: 1973, 
-		recordLabel: "UAR", stars: 4 },// we have to define each row of the array using objects
-		{ artistName: "Led Zeppelin", albumTitle: "Mothership - Deluxe Edition", genre: "Rock", pubYear: 2007, 
-		recordLabel: "Atlantic", stars: 5 },
-		{ artistName: "Kate Bush", albumTitle: "Hounds of Love", genre: "Pop", pubYear: 1985, 
-		recordLabel: "Manhattan Records", stars: 5 },
-		{ artistName: "Chance the Rapper", albumTitle: "Colouring Book", genre: "Rap/HipHop", pubYear: 2016, 
-		recordLabel: "Self-Released", stars: 5 },
-		{ artistName: "Townes Van Zandt", albumTitle: "High, Low, and In Between", genre: "Country", pubYear: 1971, 
-		recordLabel: "Poppy", stars: 4.5 },
-		{ artistName: "Manchester Orchestra", albumTitle: "Mean Everything To Nothing", genre: "Indie Rock", pubYear: 2009, 
-		recordLabel: "Favorite Gentlemen", stars: 3.5 },
-	];
+	self.about = AppSvc.about;
+	self.music = AppSvc.music;
+	self.aboutBeta = BetaSvc.about;
 };
